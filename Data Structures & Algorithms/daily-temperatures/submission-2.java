d3b4c091370484513179df7e93e48b63d72f01f1
Arvind Shelke <shelkearvind3@gmail.com>
2026-07-16 15:39:53 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        ArrayDeque<Pair> stack = new ArrayDeque<Pair>();
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > stack.peek().val) {
+                Pair poppedPair = stack.pop();
+                result[poppedPair.index] = (i - poppedPair.index);
+            }
+
+            stack.push(new Pair(temperatures[i], i));
+        }
+        return result;
+    }
+
+    class Pair {
+        int val;
+        int index;
+
+        public Pair(int val, int index) {
+            this.val = val;
+            this.index = index;
+        }
+    }
+}
